e0951fb1dce6974f4a10ff04532a544b7704e76d
Green Onyeji <50879868+ositano@users.noreply.github.com>
2025-09-26 13:56:56 +0100

    Update sw.dart

--- lib/src/i18n/sw.dart
+++ lib/src/i18n/sw.dart
@@ -12,25 +12,25 @@ class LocaleSw implements FormValidatorLocale {
 
   @override
   String maxLength(String v, int n) =>
-      'Sehemu lazima iwe na upeo wa herufi $n';
+      'Sehemu lazima isiwe na zaidi ya herufi $n';
 
   @override
-  String email(String v) => 'Sehemu si anwani halali ya barua pepe';
+  String email(String v) => 'Sehemu hii si anuani halali ya barua pepe';
 
   @override
-  String phoneNumber(String v) => 'Sehemu si nambari halali ya simu';
+  String phoneNumber(String v) => 'Sehemu hii si namba ya simu sahihi';
 
   @override
-  String required() => 'Sehemu inahitajika';
+  String required() => 'Sehemu hii inahitajika';
 
   @override
-  String ip(String v) => 'Sehemu si anwani halali ya IP';
+  String ip(String v) => 'Sehemu hii si anuani sahihi ya IP';
 
   @override
-  String ipv6(String v) => 'Sehemu si anwani halali ya IPv6';
+  String ipv6(String v) => 'Sehemu hii si anuani sahihi ya IPv6';
 
   @override
-  String url(String v) => 'Sehemu si anwani halali ya URL';
+  String url(String v) => 'Sehemu hii si anuani halali ya URL';
 
   @override
   String noMatch() => 'Thamani hazilingani';
